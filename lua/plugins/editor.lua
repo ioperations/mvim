5557a52ccca964833894b1f77f59b49270391052
aodong.qin <ioperations.c@gmail.com>
2023-03-25 22:41:59 +0800

    simplfy

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -32,22 +32,22 @@ return {
     {
       "petertriho/nvim-scrollbar",
       config = function()
-        local colors = require("tokyonight.colors").setup()
-
-        local opt = {
-          handle = {
-            color = colors.bg_highlight,
-          },
-          marks = {
-            Search = { color = colors.orange },
-            Error = { color = colors.error },
-            Warn = { color = colors.warning },
-            Info = { color = colors.info },
-            Hint = { color = colors.hint },
-            Misc = { color = colors.purple },
-          },
-        }
-        require("scrollbar").setup(opt)
+        --       local colors = require("tokyonight.colors").setup()
+        --
+        --       local opt = {
+        --         handle = {
+        --           color = colors.bg_highlight,
+        --         },
+        --         marks = {
+        --           Search = { color = colors.orange },
+        --           Error = { color = colors.error },
+        --           Warn = { color = colors.warning },
+        --           Info = { color = colors.info },
+        --           Hint = { color = colors.hint },
+        --           Misc = { color = colors.purple },
+        --         },
+        --       }
+        require("scrollbar").setup({})
       end,
     },
   },
