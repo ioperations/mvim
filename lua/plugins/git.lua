024ef665eb436559037524d229693a0a2183bf42
aodong.qin <ioperations.c@gmail.com>
2023-04-03 07:38:46 +0800

    add git-conflict.nvim

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -10,6 +10,19 @@ return {
         event = "User FileOpened",
         cmd = "Gitsigns",
     },
+    {
+        "akinsho/git-conflict.nvim",
+        config = function()
+            vim.api.nvim_exec([[ hi my_gitdiffadd guibg=#333c3e ]], false)
+            require("git-conflict").setup({
+                highlights = { -- They must have background color, otherwise the default color will be used
+                    incoming = "DiffText",
+                    current = "my_gitdiffadd",
+                },
+            })
+        end,
+    },
+
     "airblade/vim-gitgutter",
     "tpope/vim-fugitive",
 }
