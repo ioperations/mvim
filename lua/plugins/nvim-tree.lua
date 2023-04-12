71edc990ecd563303c2ba5f945d3d0c65a5817ba
aodong.qin <ioperations.c@gmail.com>
2023-04-12 18:49:53 +0800

    update nvim-tree config

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -7,7 +7,7 @@ return {
             hijack_cursor = true,
             hijack_netrw = true,
             hijack_unnamed_buffer_when_opening = false,
-            ignore_buffer_on_setup = false,
+            -- ignore_buffer_on_setup = false,
             sort_by = "name",
             root_dirs = {},
             prefer_startup_root = false,
