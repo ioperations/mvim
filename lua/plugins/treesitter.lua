5a0cfad1fe43a7961377d40573c43b1b4b9adc3f
aodong.qin <ioperations.c@gmail.com>
2023-05-25 11:09:52 +0800

    [treesitter-text-object] keymap change

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -95,16 +95,16 @@ return {
                     enable = true,
                     set_jumps = true, -- whether to set jumps in the jumplist
                     goto_next_start = {
-                        ["]m"] = "@function.outer",
-                        ["]]"] = "@class.outer",
+                        ["]]"] = "@function.outer",
+                        ["]m"] = "@class.outer",
                     },
                     goto_next_end = {
                         ["]M"] = "@function.outer",
                         ["]["] = "@class.outer",
                     },
                     goto_previous_start = {
-                        ["[m"] = "@function.outer",
-                        ["[["] = "@class.outer",
+                        ["[["] = "@function.outer",
+                        ["[m"] = "@class.outer",
                     },
                     goto_previous_end = {
                         ["[M"] = "@function.outer",
